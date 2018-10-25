56313ed67a19ffd5e8ff1c5d0bb704a337f70824
vladimir.abrahamyan96 <vladimir.abrahamyan96@gmail.com>
2018-10-26 00:44:21 +0400

    Creat Pipe searchFilter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    template: `
+        <div class="container">
+         <div class="row justify-content-center">
+            <div class="col-6">
+               <input type="text" class="form-control mb-4 mt-4" [(ngModel)]="searchCar">
+                <hr>
+                <ul class="list-group">
+                    <li class="list-group-item" *ngFor="let car of cars | carFilter:searchCar:'desc'; let i = index">
+                        <b>{{i+1}}</b> - {{car.name }} <i>{{car.desc }}</i>
+                    </li>
+                </ul>
+             </div>
+            </div>
+        </div>
+    `
 })
 export class AppComponent {
-  title = 'lesson3';
+    searchCar = '';
+    cars = [
+        {name: 'BMW', desc: 'WinPower 1'},
+        {name: 'Mercedes Benz', desc: 'WinPower 2'},
+        {name: 'Bently', desc: 'WinPower 3'},
+        {name: 'Audi', desc: 'WinPower 4'},
+        {name: 'Jeep', desc: 'WinPower 5'},
+        {name: 'Mazda', desc: 'WinPower 6'},
+        {name: 'Vaz', desc: 'WinPower 7'}
+    ]
 }
---
+++ src/app/car-filter.pipe.ts
@@ -0,0 +1,16 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({
+    name: 'carFilter'
+})
+export class CarFilterPipe implements PipeTransform {
+
+    transform(carList, searchStr: string, filedName: string) {
+        if (carList.lenght === 0 || searchStr === '') {
+            return carList;
+        }
+
+        return carList.filter(car => car[filedName].toLowerCase().indexOf(searchStr.toLowerCase()) !== -1);
+    }
+
+}
